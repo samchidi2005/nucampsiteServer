8f8024c7a4dd87ab94b8c981ffc46a33e4a81fd6
Chidiebere Nwabunike <samchidi2005@yahoo.com>
2020-06-19 02:44:17 -0500

    HTTPS

--- authenticate.js
+++ authenticate.js
@@ -38,3 +38,13 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = (req, res, next) => {
+    if (req.user.admin) {
+        return next()
+    } else {
+        const error = new Error ('You are not authorised to perform this operation!');
+        error.status = 403;
+        return next(error)
+    }
+}
